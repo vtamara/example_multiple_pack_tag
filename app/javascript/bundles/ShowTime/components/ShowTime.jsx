3f4aa5d77da6ff087b1c44955a5ab8556a786268
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-03-31 05:30:21 -0500

    Improves time component with timeZone and presents time in two different timeZones

--- app/javascript/bundles/ShowTime/components/ShowTime.jsx
+++ app/javascript/bundles/ShowTime/components/ShowTime.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 
 export default class ShowTime extends React.Component {
+  constructor(props) {
+    super(props)
+    this.locale = typeof(props.locale) != 'undefined' ? 
+      props.locale : 'es-CO'
+    this.timeZone = typeof(props.timeZone) != 'undefined' ? 
+      props.timeZone : 'America/Bogota'
+    this.state={time: new Date().toLocaleTimeString(
+      this.locale, {timeZone: this.timeZone}
+    )}
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.anotherSecond(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  anotherSecond() {
+    this.setState({
+      time: new Date().toLocaleTimeString(
+        this.locale, {timeZone: this.timeZone}
+      )
+    })
+  }
 
   render() {
     return(
       <div>
-      {new Date().toLocaleTimeString()}
+      {this.state.time} ({this.timeZone})
       </div>
     )
   }
